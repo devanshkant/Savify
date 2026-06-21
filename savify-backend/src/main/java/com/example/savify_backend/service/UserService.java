a2cbfbf99004298423d0ff90ec3cb5ecf349ae7c
devanshkant <devanshkantkashyap@gmail.com>
2026-06-21 21:25:25 +0530

    removed unused import

--- savify-backend/src/main/java/com/example/savify_backend/service/UserService.java
+++ savify-backend/src/main/java/com/example/savify_backend/service/UserService.java
@@ -3,7 +3,6 @@ package com.example.savify_backend.service;
 import com.example.savify_backend.dto.LoginRequest;
 import com.example.savify_backend.dto.RegisterRequest;
 import com.example.savify_backend.entities.User;
-import org.springframework.stereotype.Service;
 
 public interface UserService {
     User registerUser(RegisterRequest request);
